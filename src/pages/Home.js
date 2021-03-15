a79b49cce128a769c6aa99984f5bd5fb23119b9c
Rashmita Khatri <rashmita.khatri@mavs.uta.edu>
2021-03-15 16:38:25 -0500

    header elements

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Home.css';
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps"
+import { Avatar } from '@material-ui/core'
+
+function Home() {
+    return (
+        <div className = 'home'>
+            
+            <div className="home_header">
+                <div className="header_left">
+                <Link to='/about'>About</Link>
+                <Link to='/store'>Store</Link>
+                
+                </div>
+                <div className="header_right">
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+                </div>
+            </div>
+
+            <div className="home_body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
